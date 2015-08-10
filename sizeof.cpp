672734f53df0edd1cb3aa3569a0cf924da2efcae
howpenghui <phow1@stanford.edu>
2015-08-10 13:39:32 -0700

    sizeof print

---
+++ sizeof.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(){
+	cout << "sizeof(char): " << sizeof(char) << endl;
+	cout << "sizeof(unsigned long long): " << sizeof(unsigned long long) << endl;
+	cout << "sizeof(int): " << sizeof(int) << endl;
+	cout << "sizeof(unsigned long): " << sizeof(unsigned long) << endl;
+	cout << "sizeof(unsigned int): " << sizeof(unsigned int) << endl;
+	cout << "sizeof(bool): " << sizeof(bool) << endl;
+}
